e5b7d4d900e4dcdec5aa62b7adf29dfc05c82681
aynsantos <allyson.santos@live.com>
2023-04-06 21:03:31 -0300

    Project created

---
+++ src/test/java/api/attornatus/person/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.attornatus.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
