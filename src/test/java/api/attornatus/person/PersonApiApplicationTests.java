238406a3b285905356741335258be8bcce6c2e40
aynsantos <allyson.santos@live.com>
2023-04-13 16:38:06 -0300

    Test implementation

--- src/test/java/api/attornatus/person/PersonApiApplicationTests.java
+++ src/test/java/api/attornatus/person/PersonApiApplicationTests.java
@@ -1,13 +1,32 @@
 package api.attornatus.person;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PersonApiApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	private int randomPort;
+
+	@BeforeEach
+	public void setUpTest(){
+		System.out.println(randomPort);
+		RestAssured.port = randomPort;
 	}
 
+	@Test
+	void whenFindAllCheckResult() {
+		RestAssured.given()
+				.when()
+				.get("/person")
+				.then()
+				.statusCode(HttpStatus.OK.value())
+				.extract().response().body().prettyPrint();
+
+	}
 }
